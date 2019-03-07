e01e1f217639f8fe57f83be7d864734c63edd336
Jorge Henrique <jorgehasd@gmail.com>
2019-03-07 08:03:52 -0300

    refactoring to common service interface

---
+++ pet-clinic-data/src/main/java/services/OwnerService.java
@@ -0,0 +1,8 @@
+package services;
+
+import model.Owner;
+
+public interface OwnerService extends CrudService<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+}
---
+++ pet-clinic-data/src/main/java/services/PetService.java
@@ -0,0 +1,7 @@
+package services;
+
+import model.Pet;
+
+public interface PetService extends CrudService<Pet, Long> {
+
+}
