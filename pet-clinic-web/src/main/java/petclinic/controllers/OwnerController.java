e318ba59d648be44b2dfdefd57794edc46c8e0ae
Jorge Henrique <jorgehasd@gmail.com>
2019-03-08 16:16:06 -0300

    add owner and vet lists and update controller

--- pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/OwnerController.java
@@ -1,14 +1,35 @@
 package petclinic.controllers;
 
+import model.Owner;
+import org.apache.logging.log4j.util.PropertySource;
+import org.hibernate.mapping.Collection;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import services.OwnerService;
+
+import java.util.*;
 
 @RequestMapping("owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "index", "index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        SortedSet<Owner> sortedOwners = ownerSorter(ownerService.findAll());
+        model.addAttribute("owners", sortedOwners);
         return "owners/index";
+    }
+
+    private SortedSet<Owner> ownerSorter(Set<Owner> owners){
+        SortedSet<Owner> sortedOwners = new TreeSet<>(Comparator.comparing(e-> e.getFirstName()));
+        sortedOwners.addAll(owners);
+        return sortedOwners;
     }
 }
--- pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/petclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package petclinic.controllers;
 
+import model.Vet;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/vet.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
